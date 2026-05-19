cd7ce329ca9e6a8fc6494239b5a158da5ebff2ef
Likhoded Artem <likhoded.ay@gmail.com>
2026-05-19 16:37:14 +0300

    Перенёс и переделал клавиатуры

---
+++ buttons/keyboards.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+__all__ = ["Keyboard", ""]
+
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped
+
+from buttons.buttons_schemes import *
+from callback.payload_schemes import PayloadStart
+
+Keyboard: dict[str, list[list[Button]]]
+
+
+class Keyboards:
+
+    @classmethod
+    async def change_list(
+        cls,
+        obj_uuid: UUID | Mapped[UUID],
+    ) -> Keyboard:
+        p = PayloadStart(type="list", uuid=obj_uuid, action="change")
+
+        btn1 = await CallbackButton.create("Название", p.add(inner="title"))
+        btn2 = await CallbackButton.create(
+            "Описание", p.add(inner="description")
+        )
+        btn3 = await CallbackButton.create("Тип", p.add(inner="type"))
+        btn4 = await CallbackButton.create(
+            "Содержимое", p.add(inner=["values", "start"])
+        )
+        btn5 = await CallbackButton.create(
+            "🗑Удалить", p.add(action="delete", inner="start", cancel_inner=True)
+        )
+
+        keyboard = [[btn1, btn2], [btn3, btn4], [btn5]]
+
+        return {"buttons": keyboard}
+
+    @classmethod
+    async def reg(cls) -> Keyboard:
+        btn1 = await RequestContactButton.create("Отправить данные")
+
+        keyboard = [[btn1]]
+
+        return {"buttons": keyboard}
+
+    @classmethod
+    async def yes_no(
+        cls,
+        type: str,
+        obj_uuid: UUID,
+        action: str,
+        inner: str | list[str] | None = None,
+    ) -> Keyboard:
+        p = PayloadStart(type=type, uuid=obj_uuid, action=action, inner=inner)
+
+        btn1 = await CallbackButton.create("✅Да", p.add(inner="yes"))
+        btn2 = await CallbackButton.create("❌Нет", p.add(inner="no"))
+
+        keyboard = [[btn1, btn2]]
+
+        return {"buttons": keyboard}
+
+    @classmethod
+    async def change_list_values(cls, obj_uuid: UUID) -> Keyboard:
+        p = PayloadStart(type="list", uuid=obj_uuid, action="change", inner="values")
+
+        btn1 = await CallbackButton.create(
+            "➕Добавить", p.add(inner=["add", "get"])
+        )
+        btn2 = await CallbackButton.create(
+            "🗑Удалить", p.add(inner=["delete", "get"])
+        )
+        btn3 = await CallbackButton.create(
+            "✏️Изменить", p.add(inner=["change", "get_id"])
+        )
+        btn4 = await CallbackButton.create("➡️Вернуться", p.add(inner="escape"))
+
+        keyboard = [[btn1, btn2], [btn3, btn4]]
+
+        return {"buttons": keyboard}
+
+    @classmethod
+    async def change_list_value(
+        cls, obj_uuid: UUID, value_id: int, came_from: str
+    ) -> Keyboard:
+        p = PayloadStart(type="list", uuid=obj_uuid, action="change", inner="value")
+
+        btn1 = await CallbackButton.create(
+            "✏️Изменить", p.add(inner=["change", f"{value_id}"])
+        )
+        btn2 = await CallbackButton.create(
+            "🗑Удалить", p.add(inner=["delete", f"{value_id}"])
+        )
+        btn3 = await CallbackButton.create(
+            "➡️Вернуться", p.add(inner=["escape", came_from])
+        )
+
+        keyboard = [[btn1, btn2], [btn3]]
+
+        return {"buttons": keyboard}
