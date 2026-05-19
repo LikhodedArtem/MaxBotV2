54cb86e2a49cf392389fdba42bd313511eb1f54a
Likhoded Artem <likhoded.ay@gmail.com>
2026-05-19 15:44:50 +0300

    Перенёс payload_functions

---
+++ callback/payload_functions.py
@@ -0,0 +1,62 @@
+"""Функции для работы с Payload"""
+
+from __future__ import annotations
+
+import re
+
+from .payload_schemes import *
+
+__all__ = ["convert_payload", "restore_payload"]
+
+
+async def convert_payload(payload: Payload) -> str:
+    """Payload -> str
+
+    Args:
+        payload: Payload для конвертирования
+
+    """
+
+    answer = f"t={payload.type}&a={payload.action}"
+
+    if payload.uuid is not None:
+        answer += f"&u={payload.uuid}"
+
+    for number, inner in enumerate(payload.inner.value):
+        answer += f"&i{number + 1}={inner}"
+
+    return answer
+
+
+async def restore_payload(payload: str) -> Payload:
+    """str -> Payload
+
+    Args:
+        payload: Payload для восстановления
+
+    """
+
+    try:
+        tags = {"t": "type", "u": "uuid", "a": "action", "i": "inner"}
+        regular = r"(?P<tag>[A-Za-z]\d*)=(?P<value>[^&=\s]+)"
+
+        data = {"type": None, "action": None, "inner": []}
+
+        find = re.findall(regular, payload)
+        for item in range(len(find)):
+            tag = find[item][0]
+            value = find[item][1]
+
+            if tag in ("t", "u", "a"):
+                data[tags[tag]] = value
+            elif bool(re.fullmatch(r"i\d+", tag)):
+                data["inner"].append(value)
+            else:
+                raise ValueError("Несуществующий ключ в Payload")
+
+        assert None not in list(data.values()), "Один из ключей отсутствует в Payload"
+
+        return Payload(**data)
+
+    except Exception as e:
+        raise ValueError(f"Неправильный вид Payload по причине: {e}")
